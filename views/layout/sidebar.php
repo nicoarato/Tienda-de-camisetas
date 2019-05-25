2071d1cf7ecba99f0810a68b79e345ed20cd6bdc
Nico Arato <nico.arato@gmail.com>
2019-05-25 17:52:39 -0300

    Visualización de menú según rol de usuario.

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -17,10 +17,16 @@
                     <h3><?=$_SESSION['identity']->nombre ." ". $_SESSION['identity']->apellido?></h3>
                 <?php endif; ?>
                     <ul>
-                        <li><a href="#">Mis pedidos</a></li>
-                        <li><a href="#">Gestionar pedidos</a></li>
-                        <li><a href="#">Gestionar categorias</a></li>
-                        <li><a href="<?=base_url?>usuario/logout">Cerrar Sesión</a></li>
+                        
+                        <?php if(isset($_SESSION['admin'])):?>
+                            <li><a href="#">Gestionar categorias</a></li>
+                            <li><a href="#">Gestionar productos</a></li>
+                            <li><a href="#">Gestionar pedidos</a></li>
+                        <?php endif; ?>
+                        <?php if(isset($_SESSION['identity'])):?>
+                            <li><a href="#">Mis pedidos</a></li>
+                            <li><a href="<?=base_url?>usuario/logout">Cerrar Sesión</a></li>
+                        <?php endif; ?>
                     </ul>
                 </div>
             </aside>
