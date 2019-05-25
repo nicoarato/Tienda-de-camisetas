9d0815babe6430b7769f435087eed573e421f679
Nico Arato <nico.arato@gmail.com>
2019-05-25 20:18:43 -0300

    Sección de categorías.

--- controllers/CategoriaController.php
+++ controllers/CategoriaController.php
@@ -1,7 +1,32 @@
 <?php
+require_once 'models/categoria.php';
+
 
 class categoriaController{
     public function index(){
-        echo "controlador categoria accion INDEX";
+        Utils::isAdmin();
+        $categoria = new Categoria();
+        $categorias = $categoria->getAll();
+        
+
+        require_once 'views/categoria/index.php';
     }
+
+    public function crear(){
+        Utils::isAdmin();
+        require_once 'views/categoria/crear.php';
+    }
+
+    public function save(){
+        Utils::isAdmin();
+       
+        if(isset($_POST) && isset($_POST['nombre'])){
+            //Guardar categoria en la base de datos
+            $categoria = new Categoria();
+            $categoria->setNombre($_POST['nombre']);
+            $categoria->save();
+            header("Location: ".base_url."categoria/index");
+        }
+    }
+
 }
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -13,19 +13,22 @@
 
                         <input type="submit" value="Enviar">
                     </form>
+                    
                 <?php else: ?>
                     <h3><?=$_SESSION['identity']->nombre ." ". $_SESSION['identity']->apellido?></h3>
                 <?php endif; ?>
                     <ul>
                         
                         <?php if(isset($_SESSION['admin'])):?>
-                            <li><a href="#">Gestionar categorias</a></li>
+                            <li><a href="<?=base_url?>categoria/index">Gestionar categorias</a></li>
                             <li><a href="#">Gestionar productos</a></li>
                             <li><a href="#">Gestionar pedidos</a></li>
                         <?php endif; ?>
                         <?php if(isset($_SESSION['identity'])):?>
                             <li><a href="#">Mis pedidos</a></li>
                             <li><a href="<?=base_url?>usuario/logout">Cerrar Sesión</a></li>
+                        <?php else: ?>
+                            <li><a href="<?=base_url?>usuario/registro">Registrarse</a></li>   
                         <?php endif; ?>
                     </ul>
                 </div>
