dd91347d00956002cf6ca4540516ed7cb3113c63
Nico Arato <nico.arato@gmail.com>
2019-05-28 02:21:36 -0300

    Estadísticas del carrito.

--- views/carrito/index.php
+++ views/carrito/index.php
@@ -33,3 +33,10 @@
     <?php endforeach; ?>
 
 </table>
+
+<div class="total-carrito">
+    <?php $stats = Utils::statsCarrito();?>
+    <h3 >Precio total: $<?=$stats['total']?></h3>
+
+    <a href="#" class="button button-pedido">Confirmar pedido</a>
+</div>
