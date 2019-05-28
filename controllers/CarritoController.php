d0511fd43bf57657365ebd04786dc533aa2bfa56
Nico Arato <nico.arato@gmail.com>
2019-05-28 01:50:51 -0300

    Listar elementos del carrito.

--- controllers/CarritoController.php
+++ controllers/CarritoController.php
@@ -3,8 +3,9 @@ require_once 'models/producto.php';
 
 class carritoController{
     public function index(){
-       var_dump($_SESSION['carrito']);
-       echo "controlador carrito accion INDEX" ;
+       $carrito = $_SESSION['carrito'];
+
+       require_once 'views/carrito/index.php';
     }
 
     public function add(){
